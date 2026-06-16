e27f3d5bc58a20549f2a71770fdcc9a801314ccb
Alex Soto <asotobu@gmail.com>
2026-06-16 12:08:49 +0200

    Guardrails

--- booking-system-backend-quarkus/src/main/java/com/galaxium/booking/ai/BookingQuestionsInputGuardrail.java
+++ booking-system-backend-quarkus/src/main/java/com/galaxium/booking/ai/BookingQuestionsInputGuardrail.java
@@ -3,13 +3,15 @@ package com.galaxium.booking.ai;
 import dev.langchain4j.data.message.UserMessage;
 import dev.langchain4j.guardrail.InputGuardrail;
 import dev.langchain4j.guardrail.InputGuardrailResult;
+import jakarta.enterprise.context.ApplicationScoped;
 
+@ApplicationScoped
 public class BookingQuestionsInputGuardrail implements InputGuardrail {
 
     @Override
     public InputGuardrailResult validate(UserMessage userMessage) {
         String text = userMessage.singleText();
-        System.out.println(text);
+        System.out.println("**** Guardrail: " + text);
         return success();
     }
 }
